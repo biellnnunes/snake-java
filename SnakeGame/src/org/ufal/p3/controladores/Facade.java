37fed562524fd60aa6bacb69b3b936c146e3d84e
Gabriel Nunes <megabriel.nunes@gmail.com>
2016-12-19 14:27:39 -0300

    Removi o A
    Remoção da letra a

--- SnakeGame/src/org/ufal/p3/controladores/Facade.java
+++ SnakeGame/src/org/ufal/p3/controladores/Facade.java
@@ -7,7 +7,7 @@ public class Facade {
 	private static Facade instancia = new Facade();
 
 	ControlColor color;
-	
+
 	public Facade() {
 		color = ControlColor.getInstance();
 	}
@@ -15,17 +15,17 @@ public class Facade {
 	public static Facade getInstancia() {
 		return instancia;
 	}
-	
+
 	public void confColor(){
 		//color.confColor();
 	}
-	
+
 	public void alterarCores(){
 		color.alterarCores();
 	}
-	
+
 	public void getColor(Atores ator){
 		//return color.getColor(ator);
 	}
-		
+
 }
